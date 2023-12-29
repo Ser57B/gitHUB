f13bb9aceb060a19140c7c487057f93dc4015983
Serge Bugaev <ua3quo@mail.ru>
2023-12-29 20:47:55 +0300

    добавлена форма

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+const form = document.forms.form;
+form.onsubmit = async (e) => {
+   e.peventDefault();
+   const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'GET',
+      headers: { 'Content-Type': 'text/plain' },
+      body: form
+   });
+};
