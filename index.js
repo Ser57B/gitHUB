66ef84bedcadfd75d5c9782309964a07b17d1a78
Serge Bugaev <ua3quo@mail.ru>
2023-12-29 21:04:26 +0300

    исправление

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const form = document.forms.form;
 form.onsubmit = async (e) => {
    e.peventDefault();
    const response = await fetch('https://jsonplaceholder.typicode.com/users', {
-      method: 'GET',
+      method: 'POST',
       headers: { 'Content-Type': 'text/plain' },
-      body: form
+      body: new FormData(form)
    });
+   const result = await response.json();
+   console.log(result);
 };
